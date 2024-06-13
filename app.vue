72d91c3b8287ddc7b487537c97888e7ce6032b6d
Yuta-Dohmae <yuta.doumae@goodsun-hd.com>
2024-06-13 17:18:37 +0900

    first

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="hello">
+    <NuxtLink to = "/">Home</NuxtLink>
+    <NuxtLink :to ="{name: 'about'}">about</NuxtLink>
+
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
